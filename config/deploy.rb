544c527f2b863729e7d5db42d586a23b62840dea
Raimund Schramm <raimund.schramm@gmx.net>
2013-08-30 18:06:52 +0200

    updates deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,52 @@
 require 'rvm/capistrano'
+require 'bundler/capistrano'
 
 set :application, 'inventory'
 
 server            '192.168.178.25', :web, :app, :db, primary: true
 set :user,        'mund'
-set :deploy_to,   "~/projects/#{application}"
+set :deploy_to,   "/home/#{user}/projects/#{application}"
 set :use_sudo,    false
-# default_run_options[:pty] = true
-# ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+# set :rvm_ruby_string, 'ruby-2.0.0-p247@inventory'
+set :rvm_ruby_string, :local
+
+set :bundle_dir, ''
+set :bundle_flags, '--system --quiet'
 
 set :scm, :git
-# set :repository,  "set your repository location here"
+# set :branch, 'master'
+set :repo_path, '/home/mund/repos/inventory.git'
+set :repository,  "#{user}@192.168.178.25:#{repo_path}"
 set :deploy_via,  :remote_cache
 
-after "deploy:restart", "deploy:cleanup"
+set :keep_releases, 1
+
+namespace :deploy do
+  task :setup_config, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.production.sample.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}"
+  end
+
+  task :symlink_database_yml, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_database_yml"
+
+  task :start, roles: :app, except: { no_release: true } do
+    run "cd #{current_path} && bundle exec thin -d -e production start -p 3068"
+  end
+
+  task :stop do
+    run "cd #{current_path} && bundle exec thin -d -e production stop"
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end
+end
+
+after "deploy", "deploy:cleanup"
