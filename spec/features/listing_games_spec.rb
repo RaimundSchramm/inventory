58ae229a19aa82792a474a558934fa0aac0ba3d9
Raimund Schramm <raimund.schramm@gmx.net>
2013-09-01 17:09:46 +0200

    adds feature 'listing games'

---
+++ spec/features/listing_games_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+feature 'Listing games' do
+  background do
+    @any_date = Time.now
+    game = Game.create({
+      name: 'Skat',
+      author: 'Author of Skat',
+      category: 'Cardgame',
+      location: 'Raimund',
+      min_players: 3,
+      max_players: 4,
+      min_playtime: 1,
+      max_playtime: nil,
+      rating: 5,
+      times_played: 100,
+      last_played: @any_date
+    })
+  end
+
+  scenario 'lists all games in inventory' do
+    visit '/games'
+
+    # page header
+    expect(page).to have_content 'Listing games'
+
+    # table header
+    expect(page).to have_selector 'th', text: 'Name'
+    expect(page).to have_selector 'th', text: 'Author'
+    expect(page).to have_selector 'th', text: 'Category'
+    expect(page).to have_selector 'th', text: 'Location'
+    expect(page).to have_selector 'th', text: 'Min players'
+    expect(page).to have_selector 'th', text: 'Max players'
+    expect(page).to have_selector 'th', text: 'Min playtime'
+    expect(page).to have_selector 'th', text: 'Max playtime'
+    expect(page).to have_selector 'th', text: 'Rating'
+    expect(page).to have_selector 'th', text: 'Times played'
+    expect(page).to have_selector 'th', text: 'Last played'
+
+    # table content
+    expect(page).to have_content 'Skat'
+    expect(page).to have_content 'Author of Skat'
+    expect(page).to have_content 'Cardgame'
+    expect(page).to have_content 'Raimund'
+    expect(page).to have_content '3'
+    expect(page).to have_content '4'
+    expect(page).to have_content '1'
+    expect(page).to have_content ''
+    expect(page).to have_content '5'
+    expect(page).to have_content '100'
+    expect(page).to have_content '2013-09-01'
+  end
+end
