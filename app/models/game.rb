3c2990ee9ac71da1f3b79bf8262ae6db23f86269
Raimund Schramm <raimund.schramm@gmx.net>
2013-09-04 17:19:04 +0200

    adds derived attribute to game model for formatting min and max players attributes into one

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ActiveRecord::Base
+
+  # derived attributes
+  def min_to_max_players
+    "#{self.min_players}-#{self.max_players}"
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'returns formatted min and max player' do
+    game = build :game, min_players: 2, max_players: 4
+    expect(game.min_to_max_players).to eq '2-4'
+  end
 end
